bea89cf96971104e8cb198faf303382b63239069
ben <ben@ben-ubuntu.(none)>
2013-01-29 13:59:16 +0800

    add travis

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,3 @@ require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 
 Rails.backtrace_cleaner.remove_silencers!
-
-# Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
